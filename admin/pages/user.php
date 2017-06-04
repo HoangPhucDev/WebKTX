1a1159a0e664d8cc6da3e5de5fc6988034ca57dd
phungnguyen1601 <nduc1601@gmail.com>
2017-06-04 16:06:57 +0700

    xong đổ dữ liệu

--- admin/pages/user.php
+++ admin/pages/user.php
@@ -17,34 +17,39 @@
               <table id="example1" class="table table-bordered table-striped">
                 <thead>
                 <tr>
-                  <th>Rendering engine</th>
-                  <th>Browser</th>
-                  <th>Platform(s)</th>
-                  <th>Engine version</th>
-                  <th>CSS grade</th>
+                  <th>Mã Người Dùng</th>
+                  <th>Mật Khẩu</th>
+                  <th>Mã Phòng</th>
+                  <th>Tên Người Dùng</th>
+                  <th>Email</th>
+                  <th>Số Điện Thoại</th>
+                  <th>Giới Tính</th>
+                  <th>Trạng Thái</th>
+                  <th>Chức Vụ</th>
                 </tr>
                 </thead>
                 <tbody>
+                <?php 
+                    include '../../class/Model.php';
+                    $data = new Model();
+                    $sql = $data->get_list("SELECT * FROM `nguoi_dung`");
+                    foreach ($sql as $row){
+                ?>
                 <tr>
-                  <td>Trident</td>
-                  <td>Internet
-                    Explorer 4.0
-                  </td>
-                  <td>Win 95+</td>
-                  <td> 4</td>
-                  <td>X</td>
-                </tr>
-                  
+                  <td><?php echo $row['MA_ND'];?></td>
+                  <td><?php echo $row['MAT_KHAU'];?></td>
+                  <td><?php echo $row['MA_PHONG'];?></td>
+                  <td><?php echo $row['TEN_ND'];?></td>
+                  <td><?php echo $row['EMAIL'];?></td>
+                  <td><?php echo $row['SDT'];?></td>
+                  <td><?php echo $row['GIOI_TINH_ND'];?></td>
+                  <td><?php echo $row['TRANG_THAI_ND'];?></td>
+                  <td><?php echo $row['CHUC_VU'];?></td>
+                </tr>  
+                <?php 
+                    }
+                ?>             
                 </tbody>
-                <tfoot>
-                <tr>
-                  <th>Rendering engine</th>
-                  <th>Browser</th>
-                  <th>Platform(s)</th>
-                  <th>Engine version</th>
-                  <th>CSS grade</th>
-                </tr>
-                </tfoot>
               </table>
             </div>
             <!-- /.box-body -->
